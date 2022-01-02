e97068aac6eb47a3fc34a2064eca42172a415a5a
y-arjun-y <arjyadav7@gmail.com>
2022-01-02 14:53:27 +0400

    fixed react error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>bettermailto</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta charSet="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:locale" content="en_US" />
         <meta
