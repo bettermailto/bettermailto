4d91b69ed50e00a35b3abedc82283d4bbc7d5b74
y-arjun-y <arjyadav7@gmail.com>
2022-02-25 14:48:04 +0400

    trying callback

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -35,4 +35,20 @@ export default NextAuth({
   },
 
   database: process.env.MONGODB_URI,
+
+  callbacks: {
+    signIn: async (profile, account) => {
+      const res = await fetch("https://api.github.com/user/emails", {
+        headers: {
+          Authorization: `token ${account.accessToken}`,
+        },
+      });
+      const emails = await res.json();
+      if (!emails || emails.length === 0) {
+        return;
+      }
+      const sortedEmails = emails.sort((a, b) => b.primary - a.primary);
+      profile.email = sortedEmails[0].email;
+    },
+  },
 });
