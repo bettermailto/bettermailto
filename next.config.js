3cfb67d0a442c67d6075349dee682631bd35bdf1
y-arjun-y <arjyadav7@gmail.com>
2022-03-12 15:57:02 +0400

    added security headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
+const { createSecureHeaders } = require("next-secure-headers");
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: createSecureHeaders({
+          referrerPolicy: "strict-origin-when-cross-origin",
+        }),
+      },
+    ];
+  },
 };
